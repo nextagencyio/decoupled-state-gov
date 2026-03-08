6a28be8905ed5cc6ed077c04c846e6d12bbf146e
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:47 -0500

    fix: mark header-dependent routes as dynamic

--- app/press-releases/page.tsx
+++ app/press-releases/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import PressReleaseCard from '../components/PressReleaseCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Press Releases | State of Cascadia',
